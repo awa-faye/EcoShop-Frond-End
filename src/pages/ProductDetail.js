309002b41f3ebbd0076a41795c584e80d3911687
awa faye <awa.faye.external@worldline.com>
2024-11-09 17:00:54 +0000

    feat: add cart

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { useCart } from '../CartContext'; 
 import '../App.css';
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const navigate = useNavigate();
+  const { addToCart } = useCart(); 
 
   useEffect(() => {
     const fetchProductDetail = async () => {
@@ -13,10 +15,10 @@ function ProductDetail() {
         const response = await fetch('/products.json');
         if (!response.ok) throw new Error('Network response was not ok');
         const products = await response.json();
-        const selectedProduct = products.find(p => p.id === parseInt(id));
+        const selectedProduct = products.find((p) => p.id === parseInt(id));
         setProduct(selectedProduct);
       } catch (error) {
-        console.error("Error fetching product:", error);
+        console.error('Error fetching product:', error);
       }
     };
 
@@ -27,7 +29,9 @@ function ProductDetail() {
 
   return (
     <div className="product-detail">
-      <button className="back-button" onClick={() => navigate(-1)}>Back to Products</button>
+      <button className="back-button" onClick={() => navigate(-1)}>
+        Back to Products
+      </button>
       <div className="product-detail-content">
         <img src={product.image} alt={product.name} className="product-image-detail" />
         <div className="product-info">
@@ -36,10 +40,14 @@ function ProductDetail() {
           <p className="product-description">{product.description}</p>
           <div className="star-rating">
             {[...Array(5)].map((_, index) => (
-              <span key={index} className={index < product.rating ? 'star filled' : 'star'}>★</span>
+              <span key={index} className={index < product.rating ? 'star filled' : 'star'}>
+                ★
+              </span>
             ))}
           </div>
-          <button className="add-to-cart-button">Add to Cart</button>
+          <button className="add-to-cart-button" onClick={() => addToCart(product)}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
